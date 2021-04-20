aa0ccff08bdba29d5e278bc1bfad3a730311b557
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-04-20 23:42:45 +0530

    updated desktop version of Products

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -5,7 +5,7 @@ import { ProductOperations } from './components/ProductOperations';
 import styles from './Products.module.css';
 
 export function Products({ setRoute }) {
-  const { showInventoryAll, showFastDelivery, sortBy, priceSlider } = useData();
+  const { showInventoryAll, showFastDelivery, sortBy, priceSlider, dataDispatch } = useData();
 
   function getSortedData(productList, sortBy) {
     if (sortBy && sortBy === 'PRICE_HIGH_TO_LOW') {
@@ -43,9 +43,100 @@ export function Products({ setRoute }) {
         </div>
       ) : (
         <div className={styles.container}>
-          {filteredData.map((product) => (
-            <ProductCard product={product} setRoute={setRoute} />
-          ))}
+          <div className={styles.sidebar}>
+            <h3>Filter</h3>
+            <h4>Sort By</h4>
+            <ul>
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="PRICE_HIGH_TO_LOW"
+                  onChange={() =>
+                    dataDispatch({
+                      type: 'SORT',
+                      payload: 'PRICE_HIGH_TO_LOW',
+                    })
+                  }
+                />
+                <label htmlFor="PRICE_HIGH_TO_LOW"> Price: High-Low </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="PRICE_LOW_TO_HIGH"
+                  onChange={() =>
+                    dataDispatch({
+                      type: 'SORT',
+                      payload: 'PRICE_LOW_TO_HIGH',
+                    })
+                  }
+                />
+                <label htmlFor="PRICE_LOW_TO_HIGH"> Price: Low-High </label>
+              </li>
+            </ul>
+            <h4>Availability</h4>
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  name="TOGGLE_INVENTORY"
+                  id="TOGGLE_INVENTORY"
+                  checked={showInventoryAll}
+                  onChange={() =>
+                    dataDispatch({
+                      type: 'TOGGLE_INVENTORY',
+                    })
+                  }
+                />
+                <label htmlFor="TOGGLE_INVENTORY"> Include Out Of Stock </label>
+              </li>
+            </ul>
+            <h4>Delivery</h4>
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  name="TOGGLE_DELIVERY"
+                  id="TOGGLE_DELIVERY"
+                  checked={showFastDelivery}
+                  onChange={() =>
+                    dataDispatch({
+                      type: 'TOGGLE_DELIVERY',
+                    })
+                  }
+                />
+                <label htmlFor="TOGGLE_DELIVERY"> Fast Delivery Only </label>
+              </li>
+            </ul>
+            <h4>Price</h4>
+            <ul className={styles.priceSlider}>
+              <li> ₹0 - ₹{priceSlider}</li>
+              <li>
+                <input
+                  id="PRICE_RANGE"
+                  name="PRICE_RANGE"
+                  type="range"
+                  min="0"
+                  max="1000"
+                  step="100"
+                  value={priceSlider}
+                  onChange={(e) =>
+                    dataDispatch({
+                      type: 'PRICE_RANGE',
+                      payload: e.target.value,
+                    })
+                  }
+                />
+              </li>
+            </ul>
+          </div>
+          <div className={styles.productContainer}>
+            {filteredData.map((product) => (
+              <ProductCard product={product} setRoute={setRoute} />
+            ))}
+          </div>
         </div>
       )}
     </div>
