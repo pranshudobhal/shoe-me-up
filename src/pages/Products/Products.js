fe27e346072f9623f0e694906ee08337f98bc76f
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-04-29 20:04:18 +0530

    updated icon and loader

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,8 +1,8 @@
-// import { data } from '../../data';
 import { useData } from '../../context/';
 import { ProductCard } from './components/ProductCard';
 import { ProductOperations } from './components/ProductOperations';
 import styles from './Products.module.css';
+import { ProductSidebar } from './components/ProductSidebar';
 
 export function Products({ setRoute }) {
   const { products: data, showInventoryAll, showFastDelivery, sortBy, priceSlider } = useData();
@@ -35,124 +35,28 @@ export function Products({ setRoute }) {
 
   return (
     <>
-      {data.length !== 0 ? (
-        <div>
-          <ProductOperations />
-          {filteredData.length === 0 ? (
-            <div className={styles.emptyProducts}>
-              <h3>No Products available at this price</h3>
-              <p>Please broaden your price range</p>
+      <div>
+        <ProductOperations />
+        {filteredData.length === 0 && priceSlider === 0 ? (
+          <div className={styles.emptyProducts}>
+            <h3>No Products available at this price</h3>
+            <p>Please broaden your price range</p>
+          </div>
+        ) : (
+          <div className={styles.container}>
+            <ProductSidebar />
+            <div className={styles.productContainer}>
+              {data.length !== 0 ? (
+                filteredData.map((product) => <ProductCard product={product} />)
+              ) : (
+                <div className={styles.loaderContainer}>
+                  <div className={styles.loader}>Loading...</div>
+                </div>
+              )}
             </div>
-          ) : (
-            <div className={styles.container}>
-              <Sidebar />
-              <div className={styles.productContainer}>
-                {filteredData.map((product) => (
-                  <ProductCard product={product} />
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-      ) : (
-        <h1 style={{ marginTop: '5rem' }}>loadeeeeeeeeeeeeer</h1>
-      )}
+          </div>
+        )}
+      </div>
     </>
-  );
-}
-
-function Sidebar() {
-  const { showInventoryAll, showFastDelivery, priceSlider, dataDispatch } = useData();
-
-  return (
-    <div className={styles.sidebar}>
-      <h3>Filter</h3>
-      <h4>Sort By</h4>
-      <ul>
-        <li>
-          <input
-            type="radio"
-            name="sort"
-            id="PRICE_HIGH_TO_LOW"
-            onChange={() =>
-              dataDispatch({
-                type: 'SORT',
-                payload: 'PRICE_HIGH_TO_LOW',
-              })
-            }
-          />
-          <label htmlFor="PRICE_HIGH_TO_LOW"> Price: High-Low </label>
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="sort"
-            id="PRICE_LOW_TO_HIGH"
-            onChange={() =>
-              dataDispatch({
-                type: 'SORT',
-                payload: 'PRICE_LOW_TO_HIGH',
-              })
-            }
-          />
-          <label htmlFor="PRICE_LOW_TO_HIGH"> Price: Low-High </label>
-        </li>
-      </ul>
-      <h4>Availability</h4>
-      <ul>
-        <li>
-          <input
-            type="checkbox"
-            name="TOGGLE_INVENTORY"
-            id="TOGGLE_INVENTORY"
-            checked={showInventoryAll}
-            onChange={() =>
-              dataDispatch({
-                type: 'TOGGLE_INVENTORY',
-              })
-            }
-          />
-          <label htmlFor="TOGGLE_INVENTORY"> Include Out Of Stock </label>
-        </li>
-      </ul>
-      <h4>Delivery</h4>
-      <ul>
-        <li>
-          <input
-            type="checkbox"
-            name="TOGGLE_DELIVERY"
-            id="TOGGLE_DELIVERY"
-            checked={showFastDelivery}
-            onChange={() =>
-              dataDispatch({
-                type: 'TOGGLE_DELIVERY',
-              })
-            }
-          />
-          <label htmlFor="TOGGLE_DELIVERY"> Fast Delivery Only </label>
-        </li>
-      </ul>
-      <h4>Price</h4>
-      <ul className={styles.priceSlider}>
-        <li> ₹0 - ₹{priceSlider}</li>
-        <li>
-          <input
-            id="PRICE_RANGE"
-            name="PRICE_RANGE"
-            type="range"
-            min="0"
-            max="1000"
-            step="100"
-            value={priceSlider}
-            onChange={(e) =>
-              dataDispatch({
-                type: 'PRICE_RANGE',
-                payload: e.target.value,
-              })
-            }
-          />
-        </li>
-      </ul>
-    </div>
   );
 }
