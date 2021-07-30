636a40a2a878971b475f85bd8642734c42f7229c
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-07-31 02:01:09 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3000';
+// export const API_URL = 'http://localhost:3000';
 export const RAZORPAY_CHECKOUT_API = 'https://checkout.razorpay.com/v1/checkout.js';
-// export const API_URL = 'https://shoemeup.pranshudobhal.repl.co';
+export const API_URL = 'https://shoemeup.pranshudobhal.repl.co';
